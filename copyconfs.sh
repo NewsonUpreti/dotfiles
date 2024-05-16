4da041b3b23c4bbdb6b9680ae775380b051af315
NewsonUpreti <upnewson@gmail.com>
2024-05-17 00:20:20 +0530

    Initializing few dotfiles

---
+++ copyconfs.sh
@@ -0,0 +1,12 @@
+
+# Copy .zshrc
+cp ~/.zshrc ~/dotfiles/.zshrc
+
+# Copy .bashrc
+cp ~/.bashrc ~/dotfiles/.bashrc
+
+# Copy Neovim configuration folder
+# cp -r ~/.config/nvim ~/.dotfiles/nvim
+
+# Copy kitty.conf
+cp ~/.config/kitty/kitty.conf ~/dotfiles/kitty.conf
